fe9e8afeaec537f0aadd40e5be47d50c9317ae5f
Edmondo Pentangelo <edmondo@gmail.com>
2021-06-17 21:09:16 +0100

    Add docker file and gracefull node exit

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const fs = require('fs')
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'content-type': 'text/html' })
   fs.createReadStream('index.html').pipe(res)
-})
+});
 
+process.on('SIGINT', function() {
+    console.log( "\nGracefully shutting down from SIGINT (Ctrl-C)" );
+    // some other closing procedures go here
+    process.exit(1);
+});
+
+console.log("Starting server at 8080");
 server.listen(process.env.PORT || 8080)
