3498dfe6367fddd84988e2976220d3f21cbec895
Edmondo Pentangelo <edmondo@gmail.com>
2021-06-18 11:53:16 +0100

    Refactor

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const port = 8080
 
 app.use('/', require('./routes'));
 
+process.on('SIGINT', function() {
+  console.log( "\nGracefully shutting down from SIGINT (Ctrl-C)" );
+  // some other closing procedures go here
+  process.exit(1);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
