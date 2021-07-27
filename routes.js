e995707c8e3be9ea74c2abb1ed10f9e284550d48
Edmondo Pentangelo <edmondo@gmail.com>
2021-07-27 17:08:48 +0100

    Added Tabs

--- routes.js
+++ routes.js
@@ -4,15 +4,36 @@ const request = require('request');
 
 const textParser = require('body-parser').text();
 
+//const url = "http://localhost:8080"
+const url = "http://junior-type.default.svc.cluster.local"
+
+router.post('/type', textParser, function(req, res) {
+    request.post({ headers: {'content-type' : 'text/plain'}
+    , url: url + "/type", body: req.body }
+    , function(error, response, body){
+        res.send(body);
+    });    
+});
+
+router.post('/compile', textParser, function(req, res) {
+    request.post({ headers: {'content-type' : 'text/plain'}
+    , url: url + "/compile", body: req.body }
+    , function(error, response, body){
+        res.send(body);
+    });    
+});
+
 router.post('/run', textParser, function(req, res) {
     request.post({ headers: {'content-type' : 'text/plain'}
-    , url: "http://junior-type.default.svc.cluster.local/run", body: req.body }
+    , url: url + "/run", body: req.body }
     , function(error, response, body){
         res.send(body);
     });    
 });
 
 module.exports = router;
+
+
 
 
  
